48d6514858437e0425c47d940b5f4012118cea43
Haim Assraf <haimassraf@gmail.com>
2026-03-04 21:23:32 +0200

    change archtet

--- rabbit.ts
+++ rabbit.ts
@@ -1,45 +1,137 @@
-import axios from 'axios';
-import amqp from 'amqplib';
+import amqp from "amqplib";
 
-async function setup() {
-    const connection = await amqp.connect('amqp://localhost');
-    const channel = await connection.createChannel();
+const DEFAULT_URL = "amqp://guest:guest@localhost:5672";
+const VHOST_URL = "amqp://guest:guest@localhost:5672/my_vhost";
 
-    const exchangeName = 'my_direct_exchange';
-    const routingKey = 'critical_logs';
-
-    await channel.assertExchange(exchangeName, 'direct', { durable: true });
-
-    const queueName = 'quorum_tasks';
-    await channel.assertQueue(queueName, {
-        durable: true,
-        arguments: { 'x-queue-type': 'quorum' }
-    });
-
-    await channel.bindQueue(queueName, exchangeName, routingKey);
-
-    return { channel, exchangeName, routingKey };
+async function connect(url: string = DEFAULT_URL) {
+  const conn = await amqp.connect(url);
+  const ch = await conn.createChannel();
+  return { conn, ch };
 }
 
-async function sendMessage(channel: amqp.Channel, exchange: string, key: string) {
-    const message = { id: 1, task: 'Haim Learn RabbitMQ!' };
+async function persistent() {
+  const { conn, ch } = await connect();
 
-    const sent = channel.publish(
-        exchange,
-        key,
-        Buffer.from(JSON.stringify(message)),
-        { persistent: true }
-    );
+  await ch.assertQueue("persistent_queue", { durable: true });
 
-    console.log(`[x] Sent: ${message.task}`);
-}
+  ch.sendToQueue("persistent_queue", Buffer.from("persistent message"), {
+    persistent: true,
+  });
 
-async function createVhost(vhostName: string) {
-    const auth = { username: 'guest', password: 'guest' };
-    try {
-        await axios.put(`http://localhost:15672/api/vhosts/${vhostName}`, {}, { auth });
-        console.log(`Vhost '${vhostName}' created successfully.`);
-    } catch (err) {
-        console.error('Error creating vhost: ', err);
+  await ch.consume("persistent_queue", (msg) => {
+    if (msg) {
+      console.log(msg.content.toString());
+      ch.ack(msg);
     }
+  });
+
+  setTimeout(() => conn.close(), 2000);
 }
+
+async function exchanges() {
+  const { conn, ch } = await connect();
+
+  await ch.assertExchange("direct_ex", "direct", { durable: true });
+  await ch.assertQueue("direct_q", { durable: true });
+  await ch.bindQueue("direct_q", "direct_ex", "info");
+  ch.publish("direct_ex", "info", Buffer.from("direct"));
+
+  await ch.assertExchange("fanout_ex", "fanout", { durable: true });
+  await ch.assertQueue("fanout_q1", { durable: true });
+  await ch.assertQueue("fanout_q2", { durable: true });
+  await ch.bindQueue("fanout_q1", "fanout_ex", "");
+  await ch.bindQueue("fanout_q2", "fanout_ex", "");
+  ch.publish("fanout_ex", "", Buffer.from("fanout"));
+
+  await ch.assertExchange("topic_ex", "topic", { durable: true });
+  await ch.assertQueue("topic_q", { durable: true });
+  await ch.bindQueue("topic_q", "topic_ex", "user.*");
+  ch.publish("topic_ex", "user.created", Buffer.from("topic"));
+
+  await ch.assertExchange("headers_ex", "headers", { durable: true });
+  await ch.assertQueue("headers_q", { durable: true });
+  await ch.bindQueue("headers_q", "headers_ex", "", {
+    "x-match": "all",
+    type: "report",
+  });
+  ch.publish("headers_ex", "", Buffer.from("headers"), {
+    headers: { type: "report" },
+  });
+
+  setTimeout(() => conn.close(), 2000);
+}
+
+async function quorum() {
+  const { conn, ch } = await connect();
+
+  await ch.assertQueue("quorum_queue", {
+    durable: true,
+    arguments: { "x-queue-type": "quorum" },
+  });
+
+  ch.sendToQueue("quorum_queue", Buffer.from("quorum message"), {
+    persistent: true,
+  });
+
+  await ch.consume("quorum_queue", (msg) => {
+    if (msg) {
+      console.log(msg.content.toString());
+      ch.ack(msg);
+    }
+  });
+
+  setTimeout(() => conn.close(), 2000);
+}
+
+async function vhost() {
+  const { conn, ch } = await connect(VHOST_URL);
+
+  await ch.assertQueue("vhost_queue", { durable: true });
+
+  ch.sendToQueue("vhost_queue", Buffer.from("vhost message"), {
+    persistent: true,
+  });
+
+  await ch.consume("vhost_queue", (msg) => {
+    if (msg) {
+      console.log(msg.content.toString());
+      ch.ack(msg);
+    }
+  });
+
+  setTimeout(() => conn.close(), 2000);
+}
+
+async function shovel() {
+  const { conn, ch } = await connect();
+
+  await ch.assertQueue("source_q", { durable: true });
+  await ch.assertQueue("dest_q", { durable: true });
+
+  ch.sendToQueue("source_q", Buffer.from("shovel message"), {
+    persistent: true,
+  });
+
+  await ch.consume("dest_q", (msg) => {
+    if (msg) {
+      console.log(msg.content.toString());
+      ch.ack(msg);
+    }
+  });
+
+  setTimeout(() => conn.close(), 4000);
+}
+
+async function run() {
+  await persistent();
+
+  await exchanges();
+
+  await quorum();
+
+  await vhost();
+
+  await shovel();
+}
+
+run()
